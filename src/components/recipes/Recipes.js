2e8a5291598baecf7f65bb0c517b3c435194eeba
akbulutesra <50598326+akbulutesra@users.noreply.github.com>
2019-08-09 20:53:44 +0200

    add recipe implemented

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Aux from '../hoc/Aux';
 import RecipeList from './recipeList/RecipeList';
 import Recipe from './recipe/Recipe';
+import ReactModal from 'react-modal';
 
 class Recipes extends Component {
 
@@ -10,6 +11,15 @@ class Recipes extends Component {
 
         this.updateSelectedIndex = this.updateSelectedIndex.bind(this);
         this.deleteRecipe = this.deleteRecipe.bind(this);
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+
+        this.handleChangeIndigrents = this.handleChangeIndigrents.bind(this);
+
+        this.handleChangeDirection = this.handleChangeDirection.bind(this);
+        this.handleAddRecipe = this.handleAddRecipe.bind(this);
 
         this.state = {
             recipes: [
@@ -24,30 +34,88 @@ class Recipes extends Component {
                     directions: "melt butter, put eggs,,,",
                 },
             ],
-            selectedIndex : 0,
+            selectedIndex: 0,
+            showModal: false,
+
+            newName: '',
+            newIndigrents: '',
+            newDirections: '',
         }
     }
 
+    handleOpenModal() {
+        this.setState({ showModal: true });
+    }
+
+    handleCloseModal() {
+        this.setState({ showModal: false });
+    }
+
     updateSelectedIndex(index) {
-        this.setState({selectedIndex: index});
+        this.setState({selectedIndex: index})
     }
 
     deleteRecipe() {
-
         const filteredArray = this.state.recipes.filter((_, i) => i !== this.state.selectedIndex);
 
-
-        this.setState({ 
+        this.setState({
             recipes: filteredArray,
-            selectedIndex : 0,
-         });
+            selectedIndex: 0,
+        });
+    }
+
+    handleChangeName(event) {
+        this.setState({ newName: event.target.value });
+    }
+
+    handleChangeIndigrents(event) {
+        this.setState({ newIndigrents: event.target.value });
+    }
+
+    handleChangeDirection(event) {
+        this.setState({ newDirections: event.target.value });
+    }
+
+    handleAddRecipe() {
+        const recipes = this.state.recipes;
+        recipes.push({
+            name: this.state.newName,
+            indigrents: this.state.newIndigrents,
+            directions: this.state.newDirections,
+        })
+
+        this.setState({
+            recipes,
+            showModal: false,
+            newName: '',
+            newIndigrents: '',
+            newDirections: '',
+        });
     }
 
     render() {
         return (
             <Aux>
-                <RecipeList value={this.state.recipes} updateMethod={this.updateSelectedIndex}/>
-                <Recipe value={this.state.recipes[this.state.selectedIndex]} delete={this.deleteRecipe}/>
+                <RecipeList value={this.state.recipes} updateMethod={this.updateSelectedIndex} />
+                <button onClick={this.handleOpenModal}>Add Recipe</button>
+                <Recipe value={this.state.recipes[this.state.selectedIndex]} delete={this.deleteRecipe} />
+
+
+
+                <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel="Minimal Modal Example"
+                >
+                    <button onClick={this.handleCloseModal}>Close Modal</button>
+                    <label>Recipe Name</label>
+                    <input type="text" value={this.state.newName} onChange={this.handleChangeName} />
+                    <label>Recipe Indigrents</label>
+                    <input type="text" value={this.state.newIndigrents} onChange={this.handleChangeIndigrents} />
+                    <label>Recipe Directions</label>
+                    <input type="text" value={this.state.newDirections} onChange={this.handleChangeDirection} />
+                    <input type="submit" value="Submit" onClick={this.handleAddRecipe} />
+
+                </ReactModal>
             </Aux>
         );
     }
